d0f60b73df41f0b5b21dc08319a57f29d97fd03c
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2024-09-07 13:56:25 -0400

    Se agrego la clase Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrosTecnicos1.Models;
+
+namespace RegistrosTecnicos1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options)
+        : base(options)
+    {
+    }
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+    public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Tecnicos>()
+            .HasOne(tt => tt.TipoTecnico)
+            .WithMany(t => t.Tecnicos)
+            .HasForeignKey(t => t.TipoTecnicoId);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
