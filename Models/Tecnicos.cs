191dab6b030fd3808561a43e86b9a09e8917a12d
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2024-09-07 13:59:31 -0400

    Se crearon las clases de Tecnicos y TiposTecnicos

---
+++ Models/Tecnicos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosTecnicos1.Models;
+
+public class Tecnicos
+{
+    [Key]
+    [Range(1, int.MaxValue, ErrorMessage = "El ID debe ser mayor o igual que 1.")]
+    public int IdTecnico { get; set; }
+    [Required(ErrorMessage = "El campo nombre es obligatorio.")]
+    public string? Nombre { get; set; }
+    [Range(0.01, double.MaxValue, ErrorMessage = "El campo sueldo por hora debe ser mayor que cero.")]
+    public double SueldoHora { get; set; }
+
+
+    public int TipoTecnicoId { get; set; }
+    public TiposTecnicos TipoTecnico { get; set; }
+}
---
+++ Models/TiposTecnicos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosTecnicos1.Models;
+
+public class TiposTecnicos
+{
+    [Key]
+    [Range(1, int.MaxValue, ErrorMessage = "El ID debe ser mayor o igual que 1.")]
+    public int TipoTecnicoId { get; set; }
+
+    [Required(ErrorMessage = "La descripción es obligatoria.")]
+    public string Descripcion { get; set; }
+    public ICollection<Tecnicos> Tecnicos { get; set; }
+}
